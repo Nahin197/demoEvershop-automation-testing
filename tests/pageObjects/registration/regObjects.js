416335705bce9ca62d5376fbb39fb236311cb3b9
Md Khademul Islam Nahin <87036714+Nahin197@users.noreply.github.com>
2026-09-01 01:31:56 +0600

    added expect properties for registration feature

--- tests/pageObjects/registration/regObjects.js
+++ tests/pageObjects/registration/regObjects.js
@@ -1,12 +1,13 @@
 export class RegistrationObjects{
      constructor(page){
         this.page = page;
-        this.accountIcon =  this.page.locator("//a[contains(@href,'account')]");
+        this.accountIcon =  this.page.locator("//a[contains(@href,'account/login')]");
         this.creatAccountLink =  this.page.locator("//a[contains(@href,'register')]");
         this.fullNameInput = this.page.getByRole("textbox",{name: 'Full Name'});
         this.emailInput = this.page.getByRole("textbox",{name: 'email'});
         this.passwordInput = this.page.getByRole("textbox",{name: 'password'});
         this.buttonSignUp = this.page.getByRole("button",{name: 'Sign Up'});
+        this.accountIconAfterLogin =  this.page.locator("//a[contains(@href,'account')]");
 
      }
 }
--- tests/specs/reg.spec.js
+++ tests/specs/reg.spec.js
@@ -1,21 +1,27 @@
-import { test } from "@playwright/test";
+import { expect, test } from "@playwright/test";
 import { Registration } from "../pageObjects/registration/regActions";
+import { RegistrationObjects } from "../pageObjects/registration/regObjects";
 
 
 test.describe("Evershop Ecommers Registration Feature",() =>{
     test("Succesfully Register with a valid information", async ({page}) => {
         const registration = new Registration(page);
+        const regLocator = new RegistrationObjects( page);
         await page.goto("https://demo.evershop.io/");
+        // await page.pause(); it is use for finding locator
         await registration.clickAccountIcon();
         await registration.clickOnCreatAccountLink();
         await page.waitForTimeout(2000);
         await registration.enterFullName("Nahin");
          await page.waitForTimeout(2000);
-        await registration.enterEmail("kha@gmail.com");
+        await registration.enterEmail("kha13@gmail.com");
          await page.waitForTimeout(2000);
         await registration.enterPassword("12345678");
          await page.waitForTimeout(2000);
         await registration.clickOnSignButton();
+        await page.waitForTimeout(5000);//here ive added 5 sec because the page loads quickly and it cant read the expected url ..so added some delay here
+        expect (await page.url()).toEqual("https://demo.evershop.io/");
+        await expect  (regLocator.accountIconAfterLogin).toBeVisible();
     });
 
     // test("Unsuccesfully Register with a valid information", async ({page}) => {});
